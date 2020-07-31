08f24fc5915c9ff726bd5c7062a9b7b4a61c7ca1
Kristina Pelevina <christya_13.07@mail.ru>
2020-07-31 12:57:24 +0600

    added StatisticServiseTest.java

--- src/test/java/ru/netology/stats/StatisticServiseTest.java
+++ src/test/java/ru/netology/stats/StatisticServiseTest.java
@@ -5,11 +5,11 @@ import ru.netology.stats.StatisticServise;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StatisticServiseTest {
+class StatisticServiceTest {
 
     @Test
     void findMax() {
-        StatisticServise service = new StatisticServise();
+        StatisticServise service = new StatisticServise()e();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
